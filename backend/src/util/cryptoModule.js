cbbc1394e9e8fb235fb3617b1bdc4ad417b19c8b
InSeong-So <goflvhxj2547@gmail.com>
2020-12-21 22:23:46 +0900

    ignore updated : properties.js

---
+++ backend/src/util/cryptoModule.js
@@ -0,0 +1,16 @@
+const {encrypts} = require("../enviroment/properties");
+const crypto = require('crypto');
+
+const sisEncrypts = password => {
+    return new Promise((resolve, reject) => {
+        crypto.scrypt(password, encrypts.getSalt(), encrypts.getLength(), encrypts.getOptions, (err, derivedKey) => {
+            if (err) reject(err);
+            resolve(derivedKey.toString("base64"));
+        });
+    });
+}
+
+sisEncrypts('테스트완료다 요것들아!!').then((result, err) => {
+    if (err) throw err;
+    console.log("The derived key is : " + result);
+});
