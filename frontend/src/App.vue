109801b3c4fe52b21dd157e26535535371683377
InSeong-So <goflvhxj2547@gmail.com>
2021-01-04 21:47:20 +0900

    템플릿 변경

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,182 +1,19 @@
 <template>
   <div id="app">
-    <div class="row">
-      <div class="leftMenu">
-        <div class="hamburger active">
-          <span></span>
-          <span></span>
-          <span></span>
+    <div class="w3-top">
+      <div class="w3-row w3-padding w3-black">
+        <div class="w3-col s3">
+          <a href="#" class="w3-button w3-block w3-black">HOME</a>
         </div>
-        <ul class="leftMenuList">
-          <li>
-            <a href="#" class="dropdown">
-              <i class="fa fa-home fa-2x" aria-hidden="true"></i>
-              <span>OASISSECURITY</span>
-            </a>
-            <ul class="dropdownlist">
-              <li>
-                <a href="#">
-                  <span>USERROLES</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>USERSANDSESSIONS</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>FUNCTIONDEFINISIONS</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>GENERALSETUP</span>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#" class="dropdown">
-              <i class="fa fa-table fa-2x" aria-hidden="true"></i>
-              <span>PATIENTMASTERINDEX</span>
-            </a>
-            <ul class="dropdownlist">
-              <li>
-                <a href="#">
-                  <span>PATIENTDEATHREGISTRATION</span>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-bed fa-2x" aria-hidden="true"></i>
-              <span>QAYSRA</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="dropdown">
-              <i class="fa fa-user-md fa-2x" aria-hidden="true"></i>
-              <span>PHDBMISC</span>
-            </a>
-            <ul class="dropdownlist">
-              <li>
-                <a href="#">
-                  <span>OBOOKING</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>ORDSERVC</span>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#" class="dropdown">
-              <i class="fa fa-heartbeat fa-2x" aria-hidden="true"></i>
-              <span>PHDREP</span>
-            </a>
-            <ul class="dropdownlist">
-              <li>
-                <a href="#" class="dropdown">
-                  <span>PHDMDREP</span>
-                </a>
-                <ul class="dropdownlist">
-                  <li>
-                    <a href="#">
-                      <span>MEDREP1</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <span>MEDREP2</span>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="dropdown">
-                  <span>PHDFINRE</span>
-                </a>
-                <ul class="dropdownlist">
-                  <li>
-                    <a href="#">
-                      <span>FINREP1</span>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#" class="dropdown">
-              <i class="fa fa-cog fa-2x" aria-hidden="true"></i>
-              <span>DATASETSETUP</span>
-            </a>
-            <ul class="dropdownlist">
-              <li>
-                <a href="#">
-                  <span>READINGBODYSITES</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>DOCUMEN</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>DSLIST</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>DATASETQUESTIONS</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>READINGTYPES</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>DATASETCLASSESTF</span>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#" class="dropdown">
-              <i class="fa fa-stethoscope fa-2x" aria-hidden="true"></i>
-              <span>TRANSCRIPTION</span>
-            </a>
-            <ul class="dropdownlist">
-              <li>
-                <a href="#">
-                  <span>TRANSCRIPTIONSETUP</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>DOCUMENTREQUISITION</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>DEFICIENCYTRACKING</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>MANAGEDOCUMENTPANEL</span>
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <div class="w3-col s3">
+          <a href="#about" class="w3-button w3-block w3-black">ABOUT</a>
+        </div>
+        <div class="w3-col s3">
+          <a href="#menu" class="w3-button w3-block w3-black">MENU</a>
+        </div>
+        <div class="w3-col s3">
+          <a href="#where" class="w3-button w3-block w3-black">WHERE</a>
+        </div>
       </div>
     </div>
     <router-view/>
@@ -196,26 +33,6 @@
     },
     methods: {
       setLeftMenu() {
-        document.querySelector(".hamburger").onclick = function () {
-          let element = document.querySelector(".leftMenu");
-          element.classList.toggle("openMenu");
-
-          let hamburger = document.querySelector(".hamburger");
-          hamburger.classList.toggle("open");
-
-          let closeAccordion = document.getElementsByClassName('dropdown');
-          let i = 0;
-          for (let i = 0; i < closeAccordion.length; i++) {
-            closeAccordion[i].classList.remove('active');
-          }
-        }
-
-        let dropdown = document.getElementsByClassName('dropdown');
-        for (let i = 0; i < dropdown.length; i++) {
-          dropdown[i].onclick = function () {
-            this.classList.toggle('active');
-          }
-        }
       },
     },
   }
