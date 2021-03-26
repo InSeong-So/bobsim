79a9478cd9ab464ad3efe5b3ee0add11a9ad64cf
sisparang <goflvhxj2547@gmail.com>
2021-03-26 23:12:32 +0900

    backend 분리 및 리팩토링 시작

--- backend/src/util/time.js
+++
@@ -1,7 +0,0 @@
-const moment = require('moment');
-
-module.exports = {
-    currentDate: moment(),
-    stringCurrentDate: moment().format(),
-    formatCurrentDate: moment().format('YYYY-MM-DD HH:mm:ss'),
-};
